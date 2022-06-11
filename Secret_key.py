facab919d3628eb5fb94a79d24369548aa699916
Ivan Petrov <lazytechwork@gmail.com>
2022-06-11 15:42:42 +0300

    Cringe

--- Secret_key.py
+++ Secret_key.py
@@ -159,21 +159,21 @@ def make_keypair():
     return private_key, public_key
 
 
-print('Curve:', curve.name)
-
-# Alice generates her own keypair.
-alice_private_key, alice_public_key = make_keypair()
-print("Alice's private key:", hex(alice_private_key))
-print("Alice's public key: (0x{:x}, 0x{:x})".format(*alice_public_key))
-
-# Bob generates his own key pair.
-bob_private_key, bob_public_key = make_keypair()
-print("Bob's private key:", hex(bob_private_key))
-print("Bob's public key: (0x{:x}, 0x{:x})".format(*bob_public_key))
-
-# Alice and Bob exchange their public keys and calculate the shared secret.
-s1 = scalar_mult(alice_private_key, bob_public_key)
-s2 = scalar_mult(bob_private_key, alice_public_key)
-assert s1 == s2
-
-print('Shared secret: (0x{:x}, 0x{:x})'.format(*s1))
+# print('Curve:', curve.name)
+#
+# # Alice generates her own keypair.
+# alice_private_key, alice_public_key = make_keypair()
+# print("Alice's private key:", hex(alice_private_key))
+# print("Alice's public key: (0x{:x}, 0x{:x})".format(*alice_public_key))
+#
+# # Bob generates his own key pair.
+# bob_private_key, bob_public_key = make_keypair()
+# print("Bob's private key:", hex(bob_private_key))
+# print("Bob's public key: (0x{:x}, 0x{:x})".format(*bob_public_key))
+#
+# # Alice and Bob exchange their public keys and calculate the shared secret.
+# s1 = scalar_mult(alice_private_key, bob_public_key)
+# s2 = scalar_mult(bob_private_key, alice_public_key)
+# assert s1 == s2
+#
+# print('Shared secret: (0x{:x}, 0x{:x})'.format(*s1))
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from tkinter import *
 
+from Secret_key import make_keypair
+
 window = Tk()
 window.title("Эллиптическая криптография")
 window.geometry('800x400')
 
-active_private_key_label = Label(window, text="Текущий приватный ключ")
-active_private_key_label.grid(column=0, row=0, pady=10)
+private_key_label = Label(window, text="Приватный ключ")
+private_key_label.grid(column=0, row=0, pady=5)
 
-choose_private_key_button = Button(window, text="Выбрать ключ")
-choose_private_key_button.grid(column=2, row=0, pady=10)
+private_key_field = Entry(window)
+private_key_field.grid(column=1, row=0, pady=5)
 
-active_private_key_text = Entry(window)
-active_private_key_text.insert(0, "./")
-active_private_key_text.grid(column=1, row=0, pady=10)
 
-generate_keypairs_button = Button(window, text="Сгенерировать пары ключей")
-generate_keypairs_button.grid(column=0, row=1, columnspan=3, pady=5)
+def generate_keypair_event():
+    priv, pub = make_keypair()
+    public_key_field.delete(0, END)
+    public_key_field.insert(0, f'{pub[0]:X}')
+    private_key_field.delete(0, END)
+    private_key_field.insert(0, f'{priv:X}')
+
+
+generate_keypair = Button(window, text="Сгенерировать пару ключей", command=generate_keypair_event)
+generate_keypair.grid(column=2, row=0, pady=5)
+
+public_key_label = Label(window, text="Публичный ключ")
+public_key_label.grid(column=0, row=1, pady=5)
+
+public_key_field = Entry(window)
+public_key_field.grid(column=1, row=1, pady=5)
 
 message_input_label = Label(window, text="Введите сообщение")
 message_input_label.grid(row=2, column=0, columnspan=2)
@@ -23,9 +36,15 @@ message_input_label.grid(row=2, column=0, columnspan=2)
 message_input = Text(window, width=50, height=6)
 message_input.grid(row=3, column=0, columnspan=2, pady=10)
 
+sign_key_label = Label(window, text="Ключ подписания")
+sign_key_label.grid(column=0, row=4, pady=5)
+
+sign_key_field = Entry(window)
+sign_key_field.grid(column=1, row=4, pady=5)
+
 encode_button = Button(window, text="Закодировать")
 decode_button = Button(window, text="Раскодировать")
-encode_button.grid(row=4, column=0)
-decode_button.grid(row=4, column=1)
+encode_button.grid(row=5, column=0)
+decode_button.grid(row=5, column=1)
 
 window.mainloop()
